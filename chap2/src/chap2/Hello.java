590489cff6149386e6dc48e1bb82470bb4a0cd91
seojinvely1004 <seojin0124@naver.com>
2022-12-07 10:38:02 +0900

    커밋 테스트

--- chap2/src/chap2/Hello.java
+++ chap2/src/chap2/Hello.java
@@ -2,7 +2,7 @@ package chap2;
 //소스파일(수동)-컴파일(자동)-실행(수동)-main메소드 내부 문장
 //java application program
 //남궁성_자바의정석 1. 자바특징 /eclipse + jdk 환경설정
-//2.변수 (46p)3.연산자 4. 조건문,반복문 
+//2.변수 (46p)3.연산자 4. 조건문,반복.
 public class Hello {
 
 	public static void main(String[] args) {
