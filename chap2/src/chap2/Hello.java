f1f9855c787c66aaa04b376a4b2419dd3597fb36
seojinvely1004 <seojin0124@naver.com>
2022-12-07 10:36:25 +0900

    feat(Java) : commit test

---
+++ chap2/src/chap2/Hello.java
@@ -0,0 +1,14 @@
+package chap2;
+//소스파일(수동)-컴파일(자동)-실행(수동)-main메소드 내부 문장
+//java application program
+//남궁성_자바의정석 1. 자바특징 /eclipse + jdk 환경설정
+//2.변수 (46p)3.연산자 4. 조건문,반복문 
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("두번째줄입니다" + "--두번째줄입니다.");
+
+	}
+
+}
